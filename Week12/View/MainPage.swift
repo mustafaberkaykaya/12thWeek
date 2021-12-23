97cba8e4907764cc3482711aa040fca52f2c9ef0
Berkay  Kaya <mberkaykaya@outlook.com>
2021-12-23 19:53:21 +0300

    add mvc folders

---
+++ Week12/View/MainPage.swift
@@ -0,0 +1,20 @@
+//
+//  MainPage.swift
+//  Week12
+//
+//  Created by Mustafa Berkay Kaya on 23.12.2021.
+//
+
+import UIKit
+
+class MainPage: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
