a2553c20bbfd129cfd0a0622746bba2be9e894fe
Berkay  Kaya <mberkaykaya@outlook.com>
2021-12-23 19:58:25 +0300

    add new uielements

--- Week12/View/MainPage.swift
+++ Week12/View/MainPage.swift
@@ -6,15 +6,89 @@
 //
 
 import UIKit
+import MobilliumBuilders
+import TinyConstraints
 
 class MainPage: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
+    private let scrollView = UIScrollView()
+    private let contentView = UIView()
+    private let textFieldUsername = UITextFieldBuilder()
+            .borderWidth(0.5)
+            .placeholder("Please enter your username.")
+            .build()
+    private let textFieldName = UITextFieldBuilder()
+            .borderWidth(0.5)
+            .placeholder("Please enter your name.")
+            .build()
+    private let textFieldSurname = UITextFieldBuilder()
+            .borderWidth(0.5)
+            .placeholder("Please enter your surname.")
+            .build()
+    private let textFieldPassword = UITextFieldBuilder()
+            .borderWidth(0.5)
+            .placeholder("Please enter your password.")
+            .isSecureTextEntry(true)
+            .build()
+    private let textFieldPasswordAgain = UITextFieldBuilder()
+            .borderWidth(0.5)
+            .placeholder("Please enter your password again.")
+            .isSecureTextEntry(true)
+            .build()
+    private let signUpButton = UIButtonBuilder()
+            .title("Sign Up")
+            .build()
+    private let mainStackView = UIStackViewBuilder()
+            .axis(.vertical)
+            .spacing(30)
+            .build()
+    
+    required init?(coder: NSCoder) {
+            super.init(coder: coder)
+            configureContents()
+        }
+        
+        override init(frame: CGRect) {
+            super.init(frame: frame)
+            configureContents()
+        }
+        
+        private func configureContents() {
+               addSubViews()
+        }
 
+}
+
+extension MainPage {
+    
+    private func addSubViews() {
+        addScrollView()
+        addMainStackView()
+        addMainContents()
+    }
+    
+    private func addScrollView() {
+        addSubview(scrollView)
+        scrollView.edgesToSuperview(usingSafeArea: true)
+        scrollView.addSubview(contentView)
+        contentView.edgesToSuperview()
+        contentView.widthToSuperview()
+    }
+    
+    private func addMainStackView() {
+        contentView.addSubview(mainStackView)
+        mainStackView.topToSuperview().constant = 25
+        mainStackView.leadingToSuperview().constant = 30
+        mainStackView.trailingToSuperview().constant = -30
+        mainStackView.bottomToSuperview().constant = -25
+    }
+    
+    private func addMainContents() {
+        mainStackView.addArrangedSubview(textFieldUsername)
+        mainStackView.addArrangedSubview(textFieldName)
+        mainStackView.addArrangedSubview(textFieldSurname)
+        mainStackView.addArrangedSubview(textFieldPassword)
+        mainStackView.addArrangedSubview(textFieldPasswordAgain)
+        mainStackView.addArrangedSubview(signUpButton)
+    }
 }
