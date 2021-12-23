e27623fe9f9780ef38d9ebddce188eb52698d9b5
Berkay  Kaya <mberkaykaya@outlook.com>
2021-12-23 23:19:09 +0300

    add pageviewcontroller

---
+++ Week12/CustomPageViewController.swift
@@ -0,0 +1,21 @@
+//
+//  CustomPageViewController.swift
+//  Week12
+//
+//  Created by Mustafa Berkay Kaya on 23.12.2021.
+//
+
+import UIKit
+
+class CustomPageViewController: UIPageViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+ 
+
+}
--- Week12/ViewController.swift
+++ Week12/ViewController.swift
@@ -9,11 +9,65 @@ import UIKit
 import MobilliumBuilders
 import TinyConstraints
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPageViewControllerDataSource, UIPageViewControllerDelegate {
 
+    var myControllers = [UIViewController]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let vc = UIViewController()
+        vc.view.backgroundColor = .red
+        myControllers.append(vc)
+        
+        let vc2 = UIViewController()
+        vc2.view.backgroundColor = .green
+        myControllers.append(vc2)
+        
+        
+       
     }
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        DispatchQueue.main.asyncAfter(deadline: .now()+2 , execute: {
+            self.presentPageVC()
+        })
+    }
+    
+    func presentPageVC() {
+        guard let first = myControllers.first else {
+            return
+        }
+        
+        let vc = UIPageViewController(transitionStyle: .pageCurl, navigationOrientation: .horizontal, options: nil)
+        vc.delegate = self
+        vc.dataSource = self
+        vc.setViewControllers([first], direction: .forward, animated: true, completion: nil)
+        present(vc, animated: true, completion: nil)
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        
+        guard let index = myControllers.firstIndex(of: viewController), index>0 else {
+            return nil
+        }
+        
+        let before = index - 1
+        
+        return myControllers[before]
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        
+        guard let index = myControllers.firstIndex(of: viewController), index<(myControllers.count-1) else {
+            return nil
+        }
+        
+        let after = index + 1
+        
+        return myControllers[after]
+        
+    }
+    
 }
