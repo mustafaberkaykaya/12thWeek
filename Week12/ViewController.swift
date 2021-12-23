db64bd060a9ff38773bf0c3bfc0ffed8b0181f3e
Berkay  Kaya <mberkaykaya@outlook.com>
2021-12-23 19:32:45 +0300

    Initial Commit

---
+++ Week12/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Week12
+//
+//  Created by Mustafa Berkay Kaya on 23.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
